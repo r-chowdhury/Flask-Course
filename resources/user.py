f835f2afbdbd9a6b4ec422de70824453b9c5219f
rc38995 <rc38995@tamu.edu>
2018-05-01 16:22:20 -0500

    testing user.py again

--- resources/user.py
+++ resources/user.py
@@ -4,26 +4,26 @@ from models.user import UserModel
 #the good thing about flask_restful and resource is that we only need to make the post method.
 
 class UserRegister(Resource):
-	parser = reqparse.RequestParser()
+    parser = reqparse.RequestParser()
 
-	parser.add_argument('username',
-		type = str,
-		required = True,
-		help = "This field cannot be left blank"
-	)
+    parser.add_argument('username',
+        type = str,
+        required = True,
+        help = "This field cannot be left blank"
+    )
 
-	parser.add_argument('password',
-		type = str,
-		required = True,
-		help = "This field cannot be left blank"
-	)
+    parser.add_argument('password',
+        type = str,
+        required = True,
+        help = "This field cannot be left blank"
+    )
 
-	def post(self):
-		data = UserRegister.parser.parse_args()
+    def post(self):
+        data = UserRegister.parser.parse_args()
 
-		if UserModel.find_by_username(data['username']):
-			return {"message": "A user with that username already exists"}, 400 #remember if we return, we won't be running the rest of it.
+        if UserModel.find_by_username(data['username']):
+            return {"message": "A user with that username already exists"}, 400 #remember if we return, we won't be running the rest of it.
 
-		user = UserModel(**data)
-		user.save_to_db()
-		return{"message": "User created successfully."}, 201
+        user = UserModel(**data)
+        user.save_to_db()
+        return{"message": "User created successfully."}, 201
